e011cdc63cb1769ec1f69c236d94880fd5063406
vishal dharmawat <geek.patel512@gmail.com>
2018-09-29 13:51:35 +0530

    prv-rm

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ module.exports = {
     'tokenexp': 36000,//7200, //in second so its 2 hour 0x43a0c3f8667e182ec02affae988ba60c0c1ff37c
     'tokenaddress': "0x74a901cd0642d2a07763e62019595454966f009b", //"0x94c7233b3ef942865e1d890482abeb314a13e803"
     'CreatorAddress': "0x80f793d184055b6d156530d36d5043a20f9805d3",
-    'CreatorAddrPrivKey': "0xF1BE9C235B2F4BBE5CF56D361355E9BA05A6C65C1764681ADC6B3129F06BE596",
     'secret': "signinKey",
     'database': 'mongodb://localhost:27017/Ranjith'
 };
